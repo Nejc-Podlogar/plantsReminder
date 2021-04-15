5e594b51953b413f8602098bcd4e3fdb763dffbf
David <dejvidkovac@gmail.com>
2021-04-15 13:22:54 +0200

    Dodano pridobivanje uporabnikovih podatkov.

--- plants_reminder/lib/profile.dart
+++ plants_reminder/lib/profile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:io' as io;
 import 'package:image_picker/image_picker.dart';
+import 'package:plants_reminder/circular_progress_indicator.dart';
+import 'package:plants_reminder/locale_database.dart';
+import 'package:plants_reminder/utility.dart';
 import 'storage.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -13,6 +16,7 @@ class Profile extends StatefulWidget {
 
 class _Profile extends State<Profile> {
   io.File _image;
+  bool _isLoading = false;
 
   Storage storage = new Storage();
 
@@ -23,6 +27,10 @@ class _Profile extends State<Profile> {
   @override
   void initState() {
     super.initState();
+    setState(() {
+      _isLoading = true;
+    });
+
     storage.readImage('profile_image').then((io.File image) {
       setState(() {
         if (image.path != '') {
@@ -30,6 +38,8 @@ class _Profile extends State<Profile> {
         }
       });
     });
+
+    _getProfileInfo();
   }
 
   _imgFromCamera() async {
@@ -56,141 +66,159 @@ class _Profile extends State<Profile> {
     image.copy('${appDir.path}/profile_image');
   }
 
+  _getProfileInfo() async {
+    Map<String, dynamic> map = {};
+    map['row_guid'] = await DatabaseHelper.getUserGuid();
+    dynamic profile =
+        await Utility.httpPostRequest(Utility.getProfileInfo, map);
+
+    print(profile);
+    // primer odgovora:
+    // {email: dejvidkovac@gmail.com, plants_count: 0, success: true, username: w3m3l}
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   void _showPicker(context) {
     showModalBottomSheet(
-        context: context,
-        builder: (BuildContext bc) {
-          return SafeArea(
-            child: Container(
-              child: new Wrap(
-                children: <Widget>[
-                  new ListTile(
-                      leading: new Icon(Icons.photo_library),
-                      title: new Text('Photo Library'),
-                      onTap: () {
-                        _imgFromGallery();
-                        Navigator.of(context).pop();
-                      }),
-                  new ListTile(
-                    leading: new Icon(Icons.photo_camera),
-                    title: new Text('Camera'),
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Container(
+            child: new Wrap(
+              children: <Widget>[
+                new ListTile(
+                    leading: new Icon(Icons.photo_library),
+                    title: new Text('Photo Library'),
                     onTap: () {
-                      _imgFromCamera();
+                      _imgFromGallery();
                       Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ),
+                    }),
+                new ListTile(
+                  leading: new Icon(Icons.photo_camera),
+                  title: new Text('Camera'),
+                  onTap: () {
+                    _imgFromCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     //return ListView();
-    return Center(
-      child: Container(
-        //color: Colors.grey[800],
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              GestureDetector(
-                  onTap: () {
-                    _showPicker(context);
-                  },
-                  child: CircleAvatar(
-                    radius: 55,
-                    //backgroundColor: Color(0xffFDCF09),
-                    backgroundColor: Colors.blue,
-                    child: _image != null
-                        ? ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.file(
-                              _image,
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.fitHeight,
-                            ),
-                          )
-                        : Container(
-                            decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(50)),
-                            width: 100,
-                            height: 100,
-                            child: Icon(
-                              Icons.camera_alt,
-                              color: Colors.grey[800],
-                            ),
-                          ),
-                  )),
-              //CircleAvatar(
-              //  foregroundImage: NetworkImage(''), // TODO: lokalna slika pol
-              //  backgroundColor: Colors.blue,
-              //  radius: 40.0,
-              //),
-              SizedBox(height: 5.0),
-              //ElevatedButton(
-              //  child: Text("Click to add/change profile photo"),
-              //  onPressed: () {}, // TODO
-              //),
-              SizedBox(height: 30.0),
-              Text(
-                "USERNAME:",
-                style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+    return _isLoading
+        ? CustomCircularProgressIndicator()
+        : Center(
+            child: Container(
+              //color: Colors.grey[800],
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    GestureDetector(
+                        onTap: () {
+                          _showPicker(context);
+                        },
+                        child: CircleAvatar(
+                          radius: 55,
+                          //backgroundColor: Color(0xffFDCF09),
+                          backgroundColor: Colors.blue,
+                          child: _image != null
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(50),
+                                  child: Image.file(
+                                    _image,
+                                    width: 100,
+                                    height: 100,
+                                    fit: BoxFit.fitHeight,
+                                  ),
+                                )
+                              : Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(50)),
+                                  width: 100,
+                                  height: 100,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.grey[800],
+                                  ),
+                                ),
+                        )),
+                    //CircleAvatar(
+                    //  foregroundImage: NetworkImage(''), // TODO: lokalna slika pol
+                    //  backgroundColor: Colors.blue,
+                    //  radius: 40.0,
+                    //),
+                    SizedBox(height: 5.0),
+                    //ElevatedButton(
+                    //  child: Text("Click to add/change profile photo"),
+                    //  onPressed: () {}, // TODO
+                    //),
+                    SizedBox(height: 30.0),
+                    Text(
+                      "USERNAME:",
+                      style: TextStyle(
+                        color: Colors.black,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 5.0),
+                    Text(
+                      "$username",
+                      style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      "E-MAIL:",
+                      style: TextStyle(
+                          color: Colors.black,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 5.0),
+                    Text(
+                      "$email",
+                      style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      "NUMBER OF PLANTS:",
+                      style: TextStyle(
+                          color: Colors.black,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 5.0),
+                    Text(
+                      "$numb_of_plants",
+                      style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.center,
               ),
-              SizedBox(height: 5.0),
-              Text(
-                "$username",
-                style: TextStyle(color: Colors.blue, fontSize: 16.0),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                "E-MAIL:",
-                style: TextStyle(
-                    color: Colors.black,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 5.0),
-              Text(
-                "$email",
-                style: TextStyle(color: Colors.blue, fontSize: 16.0),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                "NUMBER OF PLANTS:",
-                style: TextStyle(
-                    color: Colors.black,
-                    letterSpacing: 2.0,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 5.0),
-              Text(
-                "$numb_of_plants",
-                style: TextStyle(color: Colors.blue, fontSize: 16.0),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
