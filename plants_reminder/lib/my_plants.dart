a0cab572fffba9af05927c9a8154460b805a5a49
nejc-podlogar-student <nejc.podlogar@student.um.si>
2021-04-17 18:05:49 +0200

    no server - myPlants
    - prikaže samo koledar, če ni povezave na strežnik

--- plants_reminder/lib/my_plants.dart
+++ plants_reminder/lib/my_plants.dart
@@ -186,117 +186,152 @@ class _MyPlants extends State<MyPlants> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
-    return loading == false
-        ? Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              TabBar(
+    if (loading == false) {
+      if (_items == null) {
+        //Funkcija se izvede za initState ter prepreči crash, ko se izvede klic build
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Povezava na strežnik ni uspela."),
+              duration: Duration(seconds: 3),
+            ),
+          );
+        });
+        return Column(children: [
+          TableCalendar(
+              calendarStyle: CalendarStyle(
+                  todayColor: Colors.green,
+                  selectedColor: Theme.of(context).primaryColor,
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white)),
+              headerStyle: HeaderStyle(
+                formatButtonDecoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                          width: 1,
+                          color: Colors.black,
+                          style: BorderStyle.solid)),
+                ),
+                formatButtonTextStyle: TextStyle(
+                  fontSize: 12.0,
+                ),
+              ),
+              calendarController: _controller)
+        ]);
+      } else {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            TabBar(
+              controller: _nestedTabController,
+              indicatorColor: Colors.orange,
+              labelColor: Colors.orange,
+              unselectedLabelColor: Colors.black54,
+              isScrollable: true,
+              tabs: <Widget>[
+                Tab(
+                  text: "Koledar",
+                ),
+                Tab(
+                  text: "Moje rastline",
+                ),
+              ],
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.70,
+              margin: EdgeInsets.only(left: 16.0, right: 16.0),
+              child: TabBarView(
                 controller: _nestedTabController,
-                indicatorColor: Colors.orange,
-                labelColor: Colors.orange,
-                unselectedLabelColor: Colors.black54,
-                isScrollable: true,
-                tabs: <Widget>[
-                  Tab(
-                    text: "Koledar",
-                  ),
-                  Tab(
-                    text: "Moje rastline",
+                children: <Widget>[
+                  Container(
+                      child: TableCalendar(
+                          calendarStyle: CalendarStyle(
+                              todayColor: Colors.green,
+                              selectedColor: Theme.of(context).primaryColor,
+                              todayStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0,
+                                  color: Colors.white)),
+                          headerStyle: HeaderStyle(
+                            formatButtonDecoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      width: 1,
+                                      color: Colors.black,
+                                      style: BorderStyle.solid)),
+                            ),
+                            formatButtonTextStyle: TextStyle(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                          calendarController: _controller)),
+                  Container(
+                    child: GridView.count(
+                      scrollDirection: Axis.vertical,
+                      crossAxisCount: 2,
+                      children: List.generate(_items.length, (index) {
+                        return Card(
+                          child: InkWell(
+                            splashColor: Colors.blue.withAlpha(30),
+                            onTap: () {
+                              print('card tapped');
+                              _buildPopup(context, _items[index]);
+                            },
+                            child: Container(
+                              width:
+                                  MediaQuery.of(context).size.width * 0.50 - 50,
+                              height: 600,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 100,
+                                    child: Image.memory(
+                                        base64.decode(_items[index]["slika"]),
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                    0.50 -
+                                                50),
+                                  ),
+                                  Container(
+                                    height: 30,
+                                    padding: EdgeInsets.fromLTRB(5, 5, 0, 5),
+                                    child: Text(
+                                      _items[index]["name"],
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18.0),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 30,
+                                    padding: EdgeInsets.fromLTRB(5, 5, 0, 5),
+                                    child: Text(
+                                      _items[index]["watering_period"]
+                                          .toString(),
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(fontSize: 12.0),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
                   ),
                 ],
               ),
-              Container(
-                height: screenHeight * 0.70,
-                margin: EdgeInsets.only(left: 16.0, right: 16.0),
-                child: TabBarView(
-                  controller: _nestedTabController,
-                  children: <Widget>[
-                    Container(
-                        child: TableCalendar(
-                            calendarStyle: CalendarStyle(
-                                todayColor: Colors.green,
-                                selectedColor: Theme.of(context).primaryColor,
-                                todayStyle: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18.0,
-                                    color: Colors.white)),
-                            headerStyle: HeaderStyle(
-                              formatButtonDecoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        width: 1,
-                                        color: Colors.black,
-                                        style: BorderStyle.solid)),
-                              ),
-                              formatButtonTextStyle: TextStyle(
-                                fontSize: 12.0,
-                              ),
-                            ),
-                            calendarController: _controller)),
-                    Container(
-                      child: GridView.count(
-                        scrollDirection: Axis.vertical,
-                        crossAxisCount: 2,
-                        children: List.generate(_items.length, (index) {
-                          return Card(
-                            child: InkWell(
-                              splashColor: Colors.blue.withAlpha(30),
-                              onTap: () {
-                                print('card tapped');
-                                _buildPopup(context, _items[index]);
-                              },
-                              child: Container(
-                                width: screenWidth * 0.50 - 50,
-                                height: 600,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      height: 100,
-                                      child: Image.memory(
-                                          base64.decode(_items[index]["slika"]),
-                                          width: MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.50 -
-                                              50),
-                                    ),
-                                    Container(
-                                      height: 30,
-                                      padding: EdgeInsets.fromLTRB(5, 5, 0, 5),
-                                      child: Text(
-                                        _items[index]["name"],
-                                        textAlign: TextAlign.left,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18.0),
-                                      ),
-                                    ),
-                                    Container(
-                                      height: 30,
-                                      padding: EdgeInsets.fromLTRB(5, 5, 0, 5),
-                                      child: Text(
-                                        _items[index]["watering_period"]
-                                            .toString(),
-                                        textAlign: TextAlign.left,
-                                        style: TextStyle(fontSize: 12.0),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        }),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )
-        : CustomCircularProgressIndicator();
+            )
+          ],
+        );
+      }
+    } else {
+      return CustomCircularProgressIndicator();
+    }
   }
 }
